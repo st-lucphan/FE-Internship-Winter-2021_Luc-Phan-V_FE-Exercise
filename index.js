a2a4bc484d01de7bb56d292c8fda0d28419c8e51
st-lucphan <luc.phan@supremetech.vn>
2021-10-25 22:12:59 +0700

    get cart, remove cart, and increase, decrease the number of products

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function Product(id, name, image, price, discount = 0, count = 0) {
 let arr = [ new Product(1, "T-Shirt Summer Vibes", "images/image.png", 119.99, 50),
               new Product(2, "Loose Knit 3/4 Sleeve", "images/image-2.png", 119.99),
               new Product(3, "Basic Slim Fit T-Shirt", "images/image-3.png", 79.99),
-              new Product(4, "Loose Textured T-Shirt", "images/image-4.png", 119.99)]
+              new Product(4, "Loose Textured T-Shirt", "images/image-4.png", 119.99)];
 const $productList = document.querySelector('.card-list');
 const products = arr.map(function (item) {
   return `<li class="col col-3 col-sm-6">
@@ -45,5 +45,127 @@ function set_cart(id, name,image, price, discount, count) {
     }	
     cart_cur.push(product);
   };
+  localStorage.setItem('cart', JSON.stringify(cart_cur));
+  get_cart();
+}
+function render_Cart() {
+  document.body.innerHTML=`<section>
+                            <div class="container">
+                              <h3 class="cart-title">Shopping Cart</h3>
+                            </div>
+                            <div class="container table">
+                              <div class="row table-header">
+                                <div class="col col-4">
+                                  <h4>Product</h4>
+                                </div>
+                                <div class="col col-2">
+                                  <h4 class="text-center">Color</h4>
+                                </div>
+                                <div class="col col-1">
+                                  <h4 class="text-center">Size</h4>
+                                </div>
+                                <div class="col col-2">
+                                  <h4 class="text-center">Ammount</h4>
+                                </div>
+                                <div class="col col-2">
+                                  <h4 class="text-center">Price</h4>
+                                </div>
+                                <div class="col col-1">
+                                </div>
+                              </div>
+                            </div>
+                            <div class="container">
+                              <ul class="cart-list">
+                                
+                              </ul>
+                            </div>
+                            <div class="container">
+                              <div class="row checkout">
+                                <div class="col col-4">
+                                  <a href="index.html" class="back">
+                                    <img src="images/back.svg" alt="go"  class="banner-action-img">
+                                    Continue Shopping
+                                  </a>
+                                </div>
+                                <div class="col col-4">
+                                  <input type="text" class="cart-input" placeholder="Promo Code">
+                                </div>
+                                <div class="col col-2">
+                                  <span>Total cost</span>
+                                  <span class="total-cost">$159,98</span>
+                                </div>
+                                <div class="col col-2">
+                                  <button class="btn btn-warning text-uppercase">Checkout</button>
+                                </div>
+                              </div>
+                            </div>
+                          </section>`;
+  get_cart();
+}
+function get_cart(){
+  const $cartList = document.querySelector('.cart-list');
+  let cart_cur = localStorage.getItem('cart') ?? [];
+  if (cart_cur.length === 0) return;
+
+  cart_cur = JSON.parse(cart_cur);
+  const cartList=cart_cur.map(item => {
+    return `
+        <li class="row cart-item">
+          <div class="col col-4 cell justify-conent-left" >
+            <img src="${item.image}" alt="T-Shirt Summer Vibes">
+            <p class="cart-item-name">${item.name}</p>
+          </div>
+          <div class="col col-2 cell">
+            <p>White</p>
+          </div>
+          <div class="col col-1 cell">
+            <p>XL</h4>
+          </div>
+          <div class="col col-2 cell ammount">
+            <button class="decrease" onclick="decrease(${item.id})">-</button>
+            <input type="text" class="cart-input" id="count${item.id}" value="${item.count}">
+            <button class="increase" onclick="increase(${item.id})">+</button>
+          </div>
+          <div class="col col-2 cell">
+            <h4 class="price">${(item.price - item.price * item.discount / 100).toFixed(2)}</h4>
+          </div>
+          <div class="col col-1 cell">
+            <button class="btn-remove" onclick="remove_cart(${item.id})">X</button>
+          </div>
+        </li>
+    `;
+  });
+  $cartList.innerHTML=cartList.join('');
+}
+function remove_cart(id) {
+  let cart_cur = localStorage.getItem('cart') ?? [];
+  if (cart_cur.length === 0) return;
+
+  cart_cur = JSON.parse(cart_cur);
+  cart_cur = cart_cur.filter(item => item.id !== id);
+
+  localStorage.setItem('cart', JSON.stringify(cart_cur));
+  get_cart();
+}
+function increase(id){
+  let $count=document.getElementById("count"+id);
+  let cart_cur = localStorage.getItem('cart') ?? [];
+  
+  if (cart_cur.length !== 0) cart_cur = JSON.parse(cart_cur);
+  let test=cart_cur.findIndex(item=>item.id==id);
+  cart_cur[test].count=cart_cur[test].count+1;
+  console.log( cart_cur[test].count);
+  $count.value=cart_cur[test].count;
+  localStorage.setItem('cart', JSON.stringify(cart_cur));
+}
+function decrease(id){
+  let $count=document.getElementById("count"+id);
+  let cart_cur = localStorage.getItem('cart') ?? [];
+
+  if (cart_cur.length !== 0) cart_cur = JSON.parse(cart_cur);
+  let test=cart_cur.findIndex(item=>item.id==id);
+  cart_cur[test].count=cart_cur[test].count-1;
+  console.log( cart_cur[test].count);
+  $count.value=cart_cur[test].count;
   localStorage.setItem('cart', JSON.stringify(cart_cur));
 }
