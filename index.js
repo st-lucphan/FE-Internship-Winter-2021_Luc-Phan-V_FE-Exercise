71fe68895553a1c9f0ce947d36944e4abceabf03
st-lucphan <luc.phan@supremetech.vn>
2021-10-26 18:04:42 +0700

    update cart layout and add calculate total price

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const products = arr.map(function (item) {
                   ${item.discount > 0 ? `<span class="card-original-price">$${item.price}</span>` : ''}
                 </div>
                 <div class="card-hover-icons">
-                  <a data-tooltip="Add to cart" tabindex="0" onclick="set_cart(${item.id},'${item.name}','${item.image}','${item.price}','${item.discount}',1)"> <span class="icon_cart_alt">add to cart</span></a>
+                  <button class="btn btn-warning" onclick="set_cart(${item.id},'${item.name}','${item.image}','${item.price}','${item.discount}',1)"> Add to cart</button>
                 </div>
               </div>
             </li>`;
@@ -46,10 +46,48 @@ function set_cart(id, name,image, price, discount, count) {
     cart_cur.push(product);
   };
   localStorage.setItem('cart', JSON.stringify(cart_cur));
-  get_cart();
 }
+//Render cart layout
 function render_Cart() {
-  document.body.innerHTML=`<section>
+  document.body.innerHTML=`
+                            <header class="header cart-header">
+                            <div class="container">
+                              <div class="header-content header-desktop ">
+                                <div class="logo">
+                                  <a class="logo-link" href="#">
+                                    <img src="images/logo_black.svg" alt="E-shop">
+                                  </a>
+                                </div>
+                                <nav class="nav">
+                                  <ul class="nav-list">
+                                    <li class="nav-item">
+                                      <a href="#" class="cart-nav-link text-black">Men</a>
+                                    </li>
+                                    <li class="nav-item">
+                                      <a href="#" class="cart-nav-link">Women</a>
+                                    </li>
+                                    <li class="nav-item">
+                                      <a href="#" class="cart-nav-link">Kids</a>
+                                    </li>
+                                  </ul>
+                                </nav>
+                                <div class="option">
+                                  <ul class="option-list">
+                                    <li class="option-item">
+                                      <a href="#" class="option-link"><img src="images/search_black.svg" alt="search"></a>
+                                    </li>
+                                    <li class="option-item">
+                                      <a href="#" class="option-link"><img src="images/cart_black.svg" alt="cart" onclick="render_Cart()"></a>
+                                    </li>
+                                    <li class="option-item">
+                                      <a href="#" class="option-link"><img src="images/avatar_black.svg" alt="avatar"></a>
+                                    </li>
+                                  </ul>
+                                </div>
+                              </div>
+                            </div>
+                          </header>
+                          <section class="section section-cart">
                             <div class="container">
                               <h3 class="cart-title">Shopping Cart</h3>
                             </div>
@@ -88,27 +126,37 @@ function render_Cart() {
                                   </a>
                                 </div>
                                 <div class="col col-4">
-                                  <input type="text" class="cart-input" placeholder="Promo Code">
+                                  <div class="promote-code">
+                                    <input type="text" class="promote-input" placeholder="Promo Code">
+                                    <img src="images/send.svg" class="send-img" alt="Send">
+                                  </div>
                                 </div>
                                 <div class="col col-2">
                                   <span>Total cost</span>
-                                  <span class="total-cost">$159,98</span>
+                                  <span class="total-cost">$0,00</span>
                                 </div>
                                 <div class="col col-2">
                                   <button class="btn btn-warning text-uppercase">Checkout</button>
                                 </div>
                               </div>
                             </div>
-                          </section>`;
+                          </section>`; 
+  const $totalPrice = document.querySelector(".total-cost");
+  let cart_cur = localStorage.getItem('cart') ?? [];
+  if (cart_cur.length === 0) return;
+  cart_cur = JSON.parse(cart_cur);
+  let total = calculateTotalPrice(cart_cur);
+  $totalPrice.innerHTML=`$${total}`;                       
   get_cart();
 }
+//render cart-list
 function get_cart(){
   const $cartList = document.querySelector('.cart-list');
   let cart_cur = localStorage.getItem('cart') ?? [];
+  
   if (cart_cur.length === 0) return;
-
   cart_cur = JSON.parse(cart_cur);
-  const cartList=cart_cur.map(item => {
+  const List=cart_cur.map(item => {
     return `
         <li class="row cart-item">
           <div class="col col-4 cell justify-conent-left" >
@@ -116,56 +164,72 @@ function get_cart(){
             <p class="cart-item-name">${item.name}</p>
           </div>
           <div class="col col-2 cell">
-            <p>White</p>
+            <p class="text-bold">White</p>
           </div>
           <div class="col col-1 cell">
-            <p>XL</h4>
+            <p class="text-bold">XL</h4>
           </div>
           <div class="col col-2 cell ammount">
             <button class="decrease" onclick="decrease(${item.id})">-</button>
-            <input type="text" class="cart-input" id="count${item.id}" value="${item.count}">
-            <button class="increase" onclick="increase(${item.id})">+</button>
+            <input type="text" class="ammount-input" id="count${item.id}" value="${item.count}">
+            <button class="increase text-bold" onclick="increase(${item.id})">+</button>
           </div>
           <div class="col col-2 cell">
             <h4 class="price">${(item.price - item.price * item.discount / 100).toFixed(2)}</h4>
           </div>
           <div class="col col-1 cell">
-            <button class="btn-remove" onclick="remove_cart(${item.id})">X</button>
+            <button class="btn-remove text-bold" onclick="remove_cart(${item.id})">X</button>
           </div>
         </li>
     `;
   });
-  $cartList.innerHTML=cartList.join('');
+  $cartList.innerHTML=List.join('');
 }
+//Delete cart-item
 function remove_cart(id) {
   let cart_cur = localStorage.getItem('cart') ?? [];
+  let $totalPrice = document.querySelector(".total-cost");
+  
   if (cart_cur.length === 0) return;
-
   cart_cur = JSON.parse(cart_cur);
   cart_cur = cart_cur.filter(item => item.id !== id);
-
+  let total = calculateTotalPrice(cart_cur);
+  $totalPrice.innerHTML=`$${total}`;
   localStorage.setItem('cart', JSON.stringify(cart_cur));
   get_cart();
 }
+// increase ammount of product
 function increase(id){
   let $count=document.getElementById("count"+id);
   let cart_cur = localStorage.getItem('cart') ?? [];
+  let $totalPrice = document.querySelector(".total-cost");
   
   if (cart_cur.length !== 0) cart_cur = JSON.parse(cart_cur);
   let test=cart_cur.findIndex(item=>item.id==id);
   cart_cur[test].count=cart_cur[test].count+1;
   console.log( cart_cur[test].count);
   $count.value=cart_cur[test].count;
+  let total = calculateTotalPrice(cart_cur);
+  $totalPrice.innerHTML=`$${total}`;
   localStorage.setItem('cart', JSON.stringify(cart_cur));
 }
+// decrease ammount of product
 function decrease(id){
   let $count=document.getElementById("count"+id);
   let cart_cur = localStorage.getItem('cart') ?? [];
+  let $totalPrice = document.querySelector(".total-cost");
 
   if (cart_cur.length !== 0) cart_cur = JSON.parse(cart_cur);
   let test=cart_cur.findIndex(item=>item.id==id);
   cart_cur[test].count=cart_cur[test].count-1;
   console.log( cart_cur[test].count);
   $count.value=cart_cur[test].count;
+  let total = calculateTotalPrice(cart_cur);
+  $totalPrice.innerHTML=`$${total}`;
   localStorage.setItem('cart', JSON.stringify(cart_cur));
+}
+// calcalate total price
+function calculateTotalPrice(cart_cur){
+  const total=cart_cur.reduce((total, item) => item.count*(item.price-item.price*item.discount/100) + total, 0);
+  return total.toFixed(2);
 }
